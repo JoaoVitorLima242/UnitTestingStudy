49f5a41154e49c82dc789ca836d30bdd600c4c42
JoaoVitorLima242 <jlima0500@gmail.com>
2022-07-02 17:45:26 -0300

    new test and comments

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+// ANOTACOES
+
+/* 
+    GET - Caso o elemento nao esteja na tela ele vai dar erro!
+    QUERY - Caso o elemento nao esteja na tela ele NAO vai dar o erro
+    --> Muito usado para testar se o elemento nao esta na tela.
+*/
